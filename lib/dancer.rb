140afe354dd2a7f767a218cd91c35bff361f728d
Derric Fragosa <fderric@gmail.com>
2019-11-18 06:14:09 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,9 +4,8 @@ require_relative './fancy_dance.rb'
 
 class Dancer
   extend FancyDance::ClassMethods
-  include 
-  FancyDance::InstanceMethods
-end 
+  include FancyDance::InstanceMethods
+end
  
   
   attr_accessor :name 
