b2b78c9e45ad28d4984e3ff848a6d40cdaa67de5
Derric Fragosa <fderric@gmail.com>
2019-11-18 06:15:28 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,11 +2,11 @@
 # require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 
-class Kid 
+class Kid
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
-end 
-  
+end
+
   attr_accessor :name 
   
   def initialize(name)
